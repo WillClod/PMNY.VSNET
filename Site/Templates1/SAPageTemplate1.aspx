635f32be590fc83bdac8de0459a791f6e234b2cf
WillClod <72306918+WillClod@users.noreply.github.com>
2024-08-03 14:33:16 -0600

    Create Site

---
+++ Site/Templates1/SAPageTemplate1.aspx
@@ -0,0 +1,56 @@
+﻿<%@ Page 
+    Language="C#" 
+    MasterPageFile="~/Site/Site.Master" 
+    AutoEventWireup="true" 
+    CodeBehind="SAPageTemplate1.aspx.cs" 
+    Inherits="VSNetTemplates.Site.Templates1.SAPageTemplate1" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+    
+    <!-- Page Settings -->
+    <title>Template1</title>  
+    <link rel="shortcut icon" href="../../Images/IntelywareLogo.png" />
+
+    <!-- Page Style -->
+    <!-- <link href="~/Styles/PageMobile.css" rel="Stylesheet" type="text/css"/> -->
+
+    <!-- JQuery -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js" type="text/javascript"></script>
+
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"/>
+    <link href="https://getbootstrap.com/docs/5.3/assets/css/docs.css" rel="stylesheet"/>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" type="text/javascript"></script>
+
+</asp:Content>
+
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+
+    <div id="divHeader" class="divHeader">
+        
+        <div id="divButtonsHeader">
+
+        </div>
+        
+    </div>  
+            
+    <div id="divBody" class ="divBody">   
+                
+    </div>
+
+    <div id="divFooter" class ="divFooter">
+
+        <div id="divButtonsFooter">
+
+            
+        </div>
+                
+        <div id="divDataSources">
+
+
+        </div>
+
+    </div>
+    
+</asp:Content>
+
---
+++ Site/Templates1/SAPageTemplate1.aspx.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using System.Resources;
+using System.Threading;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace VSNetTemplates.Site.Templates1
+{
+    public partial class SAPageTemplate1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // Set culture.
+            Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture("es-Mx");
+            Thread.CurrentThread.CurrentUICulture = new CultureInfo("es-MX");
+
+            // On Create page...
+            if (!IsPostBack)
+            {
+                // Translation according current culture page language.
+                ficTranslation();
+            }
+        }
+
+
+        public void pcvAlert(string vpsMessage)
+        {
+            // Cleans the message to allow single quotation marks.
+            string vlsCleanMessage = vpsMessage.Replace("'", " ");
+            vlsCleanMessage = vlsCleanMessage.Replace("\n", "\\n");
+            vlsCleanMessage = vlsCleanMessage.Replace("\r", "\\r");
+
+            string vlsScript = @"<script type='text/javascript'>alert('" + vlsCleanMessage + "')</script>";
+
+            ScriptManager.RegisterStartupScript(this, typeof(Page), "alert", vlsScript, false);
+
+        }
+
+        public void pgvOpenPage(string vpsURL, bool vpbIsPopup)
+        {
+
+            if (vpbIsPopup)
+            {
+                // Open page as popup.
+                string vlsScript = "window.open('" + vpsURL + "', 'popup_window', 'width=450,height=500,left=450,top=50,resizable=no');";
+                ScriptManager.RegisterStartupScript(this, this.GetType(), "popup", vlsScript, true);
+            }
+            else {
+                // Redirect page.                                
+                Response.Redirect(vpsURL);
+            }
+
+        }
+        public int ficTranslation()
+        {
+            try
+            {
+                // Set Resource Manager class object for label traslation.
+                ResourceManager vloResourceManagerGen = new ResourceManager("VSNetTemplates.Resources.General", Assembly.GetExecutingAssembly());
+                //ResourceManager vloResourceManager = new ResourceManager("VSNetTemplates.Resources.INLoc", Assembly.GetExecutingAssembly());
+
+                // Translate labels.                
+                //gvwMain.Columns[0].HeaderText = vloResourceManager.GetString("Code");
+                //gvwMain.Columns[1].HeaderText = vloResourceManager.GetString("Name");
+                //btnNew.Text = vloResourceManagerGen.GetString("New");
+
+                // Translate Messages.
+                //hdnMsg_DuplicateCode.Value = vloResourceManagerGen.GetString("DuplicateCode");
+
+                // Assign Master Labels.
+                //MasterPage mainPage = (this.Master as MasterPage);
+                //System.Web.UI.WebControls.Label lblAppName = mainPage.FindControl("lblAppName") as System.Web.UI.WebControls.Label;
+                //lblAppName.Text = vloResourceManagerGen.GetString("Inventory"); ;
+                //System.Web.UI.WebControls.Label lblPageTitle = mainPage.FindControl("lblPageTitle") as System.Web.UI.WebControls.Label;
+                //lblPageTitle.Text = vloResourceManager.GetString("Locations"); ;
+
+            }
+            catch
+            {
+                return 1;
+            }
+
+            return 0;
+        }
+
+    }
+}
